e69e5a478f37ad3e7594d7010a0e730a6a3d7b81
Lncvrt <lncvrtreal@gmail.com>
2024-08-20 10:58:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Welcome Bot
+
+This is pretty much just a simple welcome bot that my friend wanted for a project
+
+## Setup
+
+1. Clone this repository or download the code.
+2. Run `npm install` to install the dependencies.
+3. Rename `.env.example` to `.env`.
+4. Modify the configuration options inside of `.env`.
+5. Run the bot with `node index.js`.
---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+client.once('ready', () => {
+    console.log(`The bot is now online, using ${client.user.tag}.`);
+});
+
+client.on('guildMemberAdd', member => {
+    const welcomeEmbed = new EmbedBuilder()
+        .setColor(process.env.EMBED_COLOR)
+        .setTitle(process.env.EMBED_TITLE
+            .replace("{user.ping}", `@<${member.user.id}>`)
+            .replace("{user.id}", member.user.id)
+            .replace("{user.displayName}", member.user.displayName)
+            .replace("{user.name}", member.user.username)
+            .replace("{server.name}", member.guild.name)
+            .replace("{server.memberCount}", member.guild.memberCount))
+        .setDescription(process.env.EMBED_DESCRIPTION
+            .replace("{user.ping}", `@<${member.user.id}>`)
+            .replace("{user.id}", member.user.id)
+            .replace("{user.displayName}", member.user.displayName)
+            .replace("{user.name}", member.user.username)
+            .replace("{server.name}", member.guild.name)
+            .replace("{server.memberCount}", member.guild.memberCount))
+        .setFooter({ text: process.env.EMBED_FOOTER, iconURL: process.env.EMBED_FOOTER_URL });
+
+    const channel = client.channels.cache.get(process.env.WELCOME_CHANNEL_ID);
+    if (channel) {
+        channel.send({ embeds: [welcomeEmbed] });
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
